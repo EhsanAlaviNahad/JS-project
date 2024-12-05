d7a9642a26fd9fb54b70f92207d3f7d4f4f78bbd
Ehsan AlaviNahad <alavinahadehsan@gmail.com>
2024-12-05 18:09:11 +0330

    Update main.js
    Added Quotes Inside Quotes

--- main.js
+++ main.js
@@ -363,5 +363,16 @@ let carname2 = 'Pagani'; //single quotes
 document.getElementById("demo27").innerHTML = carname1 + " " + "&" + " " + carname2;
 </script>
 
+<h3>Quotes Inside Quotes</h3>
+
+<p id="demo28"></p>
+<script>
+let answer1 = "It's alright";
+let answer2 = "He is called 'Johnny'";
+let answer3 = 'He is called "Johnny"';
+
+document.getElementById('demo28').innerHTML = 
+answer1 + "<br>" + answer2 + "<br>" + answer3;
+</script>
 </body>
 </html>
